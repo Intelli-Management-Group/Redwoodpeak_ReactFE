589c09834b19532282c4c77a83ea402cf88aa7d9
Usman Kapadiya <hirenk@intellidt.com>
2024-12-03 16:40:35 +0530

    User Update Api integration

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -2,21 +2,61 @@
 import React, { useState } from 'react';
 import ProfileView from './ProfileView';
 import ProfileEdit from './ProfileEdit';
-
+import AuthenticationServices from '../../Services/AuthenticationServices';
+import {notifyError, notifySuccess, notifyWarning} from "../Component/ToastComponents/ToastComponents";
+import { ToastContainer } from 'react-toastify';
 
 const ProfilePage = () => {
-    const [user, setUser] = useState({
-        name: 'John Doe',
-        username: '@johndoe',
-        email: 'johndoe@example.com',
-        role: '',
-    });
+    const userData = localStorage.getItem('userData');
+    const [user, setUser] = useState(JSON.parse(userData));
 
     const [isEditing, setIsEditing] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-    const handleSave = (updatedUser) => {
-        setUser(updatedUser);
-        setIsEditing(false);
+
+    const handleSave = async (formData) => {
+        console.log(formData)
+  
+        try {
+            const formdata = new FormData();
+            formdata.append("id", formData?.id ? formData?.id : "");
+            formdata.append("first_name", formData?.first_name);
+            formdata.append("last_name", formData?.last_name);
+            formdata.append("email", formData?.email);
+            if (!formData?.id) {
+            formdata.append("password", formData?.password);
+            formdata.append("confirm_password", formData?.confirmPassword);
+            }
+            formdata.append("username", formData?.username);
+            // formdata.append("name", formData?.name);
+            formdata.append("role", formData?.role);
+            formdata.append("status", formData?.id ? formData?.status : "approve");
+            formdata.append("send_user_notification", "1");
+            formdata.append("role_id", "");
+            // Call API to register user
+            const response = await AuthenticationServices.userSignUp(formdata);
+            console.log("res", response)
+            if (response?.status_code === 200) {
+              notifySuccess(formData?.id ? "User Updated SuccessFully!" : "User Created SuccessFully");
+              localStorage.setItem('userData', JSON.stringify(user));
+
+            //   setTimeout(() => {
+            //     navigate(`/usersManagement?status=all`);
+            //     onHide()
+      
+            //   }, 3000);
+      
+      
+              // if (onSave) onSave(); // Optional callback for parent component
+              // onHide();
+            } else {
+              throw new Error(response.message || "Something went wrong!");
+            }
+          } catch (error) {
+            notifyError(error.message || "Failed to register user.",);
+          } finally {
+            setLoading(false);
+          }
     };
 
     return (
@@ -26,6 +66,7 @@ const ProfilePage = () => {
             ) : (
                 <ProfileView user={user} onEdit={() => setIsEditing(true)} />
             )}
+
         </div>
     );
 };
