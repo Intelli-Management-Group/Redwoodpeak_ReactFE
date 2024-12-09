a1771fcfaf775e177f1d79d1b3b621f0a6b14cd6
Usman Kapadiya <hirenk@intellidt.com>
2024-12-09 16:35:06 +0530

    disabled add in button component

--- src/pages/Component/ButtonComponents/ButtonComponents.js
+++ src/pages/Component/ButtonComponents/ButtonComponents.js
@@ -2,13 +2,14 @@
 import React from 'react';
 
 
-const Button = ({ text, onClick, className, style, type = ""}) => {
+const Button = ({ text, disabled,onClick, className, style, type = ""}) => {
   return (
     <button 
       type={type ? type : "button"} 
       className={`btn ${className}`} 
       onClick={onClick} 
       style={style}
+      disabled={disabled}
     >
       {text}
     </button>
