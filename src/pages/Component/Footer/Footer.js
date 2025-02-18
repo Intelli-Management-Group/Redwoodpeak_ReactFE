1e6900de38b521bd49f5164c95fc7e1c7dd6f394
Usman Kapadiya <hirenk@intellidt.com>
2025-02-18 10:32:08 +0530

    Dynamic Years Set

--- src/pages/Component/Footer/Footer.js
+++ src/pages/Component/Footer/Footer.js
@@ -3,11 +3,12 @@
 import React from 'react';
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="footer ">
       <div className="container">
         <p className="text-center mb-0 contactSectionFonts">
-          Copyright © 2024 Redwoodpeak Limited. All Rights Reserved.
+          Copyright © {year} Redwoodpeak Limited. All Rights Reserved.
         </p>
       </div>
     </footer>
