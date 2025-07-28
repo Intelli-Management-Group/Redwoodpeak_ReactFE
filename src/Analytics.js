7262d15fd56e328b35402f4365e6c0fe9ca60041
Usman Kapadiya <hirenk@intellidt.com>
2025-07-29 00:37:48 +0530

    Analytic Key Update

--- src/Analytics.js
+++ src/Analytics.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
 
-const TRACKING_ID = "G-2C4X7PQEN5"; // Replace with your GA4 Measurement ID
+const TRACKING_ID = "G-ZGXVG5G9Y3"; // Replace with your GA4 Measurement ID
 
 export default function Analytics() {
   const location = useLocation();
